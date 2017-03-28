f67a1dcb4dd5660a8ad78ccab111e80d65ba5859
php-user <hayksg@gmail.com>
2017-03-29 00:16:27 +0400

    Last commit

--- module/ContactUs/src/Module.php
+++ module/ContactUs/src/Module.php
@@ -2,6 +2,9 @@
 
 namespace ContactUs;
 
+use Zend\Mail\Transport\Smtp;
+use Zend\Mail\Transport\SmtpOptions;
+
 class Module
 {
     const VERSION = '3.0.2dev';
@@ -9,5 +12,32 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                'mailTransport' => function ($container) {
+                    $config = $container->get('Config');
+                    $transport = new Smtp();
+                    $transport->setOptions(new SmtpOptions($config['mail']['transport']['options']));
+                    return $transport;
+                },
+            ],
+        ];
+    }
+
+    public function getControllerConfig()
+    {
+        return [
+            'factories' => [
+                Controller\IndexController::class => function ($container) {
+                    return new Controller\IndexController(
+                        $container->get('mailTransport')
+                    );
+                },
+            ],
+        ];
     }
 }
