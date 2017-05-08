18a2c1e09b890bd8695679d5607d891ddfa6912c
php-user <hayksg@gmail.com>
2017-05-08 18:58:05 +0400

    Last commit

--- module/Application/src/Entity/Product.php
+++ module/Application/src/Entity/Product.php
@@ -154,7 +154,6 @@ class Product
      * @Annotation\Validator({"name":"Zend\Validator\File\Extension", "options":{
      *     "extension":{"png", "jpg", "jpeg", "gif"}
      * }})
-     * @Annotation\Validator({"name":"Zend\Validator\File\IsImage"})
      * @Annotation\Validator({"name":"Zend\Validator\File\Size", "options":{"max":"20000000"}})
      * @Annotation\Input("Zend\InputFilter\FileInput")
      * @Annotation\Filter({
