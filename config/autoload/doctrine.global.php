d0772069e2fc564d765ba3b4221910db6fceab06
php-user1 <hayk.galoyan@hotmail.com>
2018-03-06 07:52:58 -0500

    Update doctrine.global.php

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -8,10 +8,10 @@ return [
             'orm_default' => [
                 'driverClass' => PDOMySqlDriver::class,
                 'params' => [
-                    'host'     => 'mysql.hostinger.in',
-                    'user'     => 'u281467562_shop',
-                    'password' => 'RCFm1mBNSOaM',
-                    'dbname'   => 'u281467562_joke',
+                    'host'     => '',
+                    'user'     => '',
+                    'password' => '',
+                    'dbname'   => '',
                 ]
             ],
         ],
